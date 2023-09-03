05bd6108a6f6392c5ca87250ad5c8bc6932db929
Vyacheslav Gordeev <v.gordeev@atg-soft.ru>
2023-09-03 18:41:46 +0300

    add GuardianDB, route /accounts/refresh_token

--- lib/auth_service_web/router.ex
+++ lib/auth_service_web/router.ex
@@ -30,6 +30,8 @@ defmodule AuthServiceWeb.Router do
   scope "/api", AuthServiceWeb do
     pipe_through [:api, :auth]
     get "/accounts/by_id/:id", AccountController, :show
+    get "/accounts/sign_out", AccountController, :sign_out
+    get "/accounts/refresh_session", AccountController, :refresh_session
     post "/accounts/update", AccountController, :update
   end
 
